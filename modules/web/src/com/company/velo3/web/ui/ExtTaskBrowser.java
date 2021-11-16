b8e907e70fd7392aa00d79d26e52a007854c1ebf
krasnovss91 <krasnovstanislav91@gmail.com>
2021-11-16 13:03:59 +0400

    view_5

--- modules/web/src/com/company/velo3/web/ui/ExtTaskBrowser.java
+++ modules/web/src/com/company/velo3/web/ui/ExtTaskBrowser.java
@@ -1,36 +1,8 @@
 package com.company.velo3.web.ui;
 
-import com.company.velo3.entity.ExTask;
-import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.thesis.web.ui.task.TaskBrowser;
 
-import javax.inject.Inject;
-import java.util.Map;
-
 public class ExtTaskBrowser extends TaskBrowser {
-     /*
-    @Inject
-    protected GroupTable<ExTask> taskTable;//название как в дескрипторе
 
-    @Override
-    public void init(Map params){
-     if(taskTable.getSelected().size() > 10){
-        showNotification("Не более 10 строк!");
-     }
-    }
-
-
-public class CustomerBrowse extends AbstractLookup {
-   @Inject
-    protected Table<Customer> table;
-
-    public void onEditStatusBtn(Component source) {
-        if (table.getSelected().isEmpty()) {
-            showNotification("Select detail rows first", NotificationType.HUMANIZED);
-            return;
-        }
-    }
-}
-     */
 
 }
